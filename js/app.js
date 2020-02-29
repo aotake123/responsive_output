c172b69c6e3617f60a53870e53d6e9927feafcc3
Tasuku Ohki <>
2020-02-29 12:16:55 +0900

    新規追加　ハンバーガーメニュー

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(function(){
+
+  // SPメニュー
+  $('.js-toggle-sp-menu').on('click', function () {
+    $(this).toggleClass('active');
+    $('.js-toggle-sp-menu-target').toggleClass('active');
+  });
+
+  //SPメニューの展開後のリンクを押した際に戻す操作
+  $('.js-toggle-menu-link').on('click', function(){
+    $('.js-toggle-sp-menu').removeClass('active');
+    $('html').removeClass('menu');
+    $('.js-toggle-sp-menu-target').removeClass('active');
+  });
+
+});
+
